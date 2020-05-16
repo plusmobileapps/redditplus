b42d606c8eac7f9a2154796fce179cb0c03a7b1b
Andrew Steinmetz <andrew@plusmobileapps.com>
2020-05-16 00:49:19 -0700

    slide botttom nav up and down on destination change

--- app/src/main/java/com/plusmobileapps/reddit/MainActivity.kt
+++ app/src/main/java/com/plusmobileapps/reddit/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.plusmobileapps.reddit
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,6 +10,8 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottom_navigation: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +25,41 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        bottom_navigation = findViewById(R.id.nav_view)
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications -> showBottomNavigation()
+                R.id.postOptionsBottomSheet -> Unit
+                else -> hideBottomNavigation()
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return findNavController(R.id.nav_host_fragment).navigateUp()
+    }
+
+    private fun hideBottomNavigation() {
+        // bottom_navigation is BottomNavigationView
+        with(bottom_navigation) {
+            if (visibility == View.VISIBLE && alpha == 1f) {
+                animate()
+                    .translationY(300f)
+                    .withEndAction { visibility = View.GONE }
+                    .duration = 400
+            }
+        }
+    }
+
+    private fun showBottomNavigation() {
+        // bottom_navigation is BottomNavigationView
+        with(bottom_navigation) {
+            visibility = View.VISIBLE
+            animate()
+                .translationY(0f)
+                .duration = 400
+        }
     }
 }
